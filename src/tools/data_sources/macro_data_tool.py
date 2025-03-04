07f9be63a6cb7dd0f15ed323d2d298eebfe2487f
iAmGiG <gigachris1@hotmail.com>
2025-03-03 21:35:33 -0500

    tested out the api key, connects but more testing needed.

--- src/tools/data_sources/macro_data_tool.py
+++ src/tools/data_sources/macro_data_tool.py
@@ -10,6 +10,8 @@ class MacroDataTool:
 
 
 # Example Usage
-macro_tool = MacroDataTool("YOUR_FRED_API_KEY")
+macro_tool = MacroDataTool("keyhere")
 gdp_data = macro_tool.get_gdp()
 print(gdp_data.head())
+# this print line VVV is required to be displayed when developing applications.
+print("This product uses the FRED® API but is not endorsed or certified by the Federal Reserve Bank of St. Louis.")
