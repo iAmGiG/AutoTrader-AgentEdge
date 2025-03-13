cca78f460673b36c328fcfb895b2141b786f0349
WormsCanned <gigachris1@hotmail.com>
2025-03-13 16:49:16 -0400

    Boilerplate

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RH2MAS",
+    version="0.1",
+    packages=find_packages(),
+    # If your config.json or other data files need to be included, see MANIFEST.in usage
+)
